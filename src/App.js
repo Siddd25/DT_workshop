69280526e4c15895b45f28ca227020c0fe5f708f
Siddhesh Madkaikar <siddheshmadkaikar25@gmail.com>
2025-09-17 18:40:11 +0530

    Background changes

--- src/App.js
+++ src/App.js
@@ -688,7 +688,7 @@ function App() {
           playsInline
           className="absolute top-0 left-0 w-full h-full object-cover z-0"
         >
-          <source src={process.env.PUBLIC_URL + '/images/bg_vid.mp4'} type="video/mp4" />
+          <source src={process.env.PUBLIC_URL + '/images/vid.mp4'} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
